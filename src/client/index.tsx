3bb30cd5cba058dd173bff793ae77acedd4c9fc4
Esteban Santini <esantinie@gmail.com>
2017-08-25 16:09:23 -0700

    get css from /public/

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import allReducers from "./app/reducers/AllReducers";
 
 // import "file-loader?name=[name].[ext]!../public/favicon.ico";
 // import "file-loader?name=[name].[ext]!../public/index.html";
 // import "file-loader?name=[name].[ext]!../public/manifest.json";
 
-import "file-loader?name=[name].css!./css/style.less";
+import "file-loader?name=[name].css!./public/style.less";
 
 import App from "./app/App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(allReducers);
+
+ReactDOM.render((
+		// <Provider store={store}>
+			<App/>
+		// </Provider>
+	), document.getElementById("root"));
+
 registerServiceWorker();
