53b6184ebe91d49ec64db7ad85f857b8cc910daf
Esteban Santini <esantinie@gmail.com>
2017-09-05 17:13:02 -0700

    Fetch & store rates from openexchangerates.org, but still using old hardcoded rates.

--- src/client/app/Store.ts
+++ src/client/app/Store.ts
@@ -1,6 +1,23 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, AnyAction } from "redux";
+import thunk from "redux-thunk";
 import logger from "redux-logger";
+import rxjs from "rxjs";
 
 import allReducers from "./reducers/AllReducers";
 
-export default createStore(allReducers, {}, applyMiddleware(logger));
+const store = createStore(allReducers, {}, applyMiddleware(thunk, logger));
+
+store.dispatch((dispatch: (action: AnyAction) => void ): any => {
+
+	fetch("https://openexchangerates.org/api/latest.json?app_id=192732a1f5374bbc9671ea6b3a6759c9")
+		.then((response) => {
+			if (response.status === 200) {
+				return response.json();
+			}
+		})
+		.then((response) => {
+			dispatch({ type: "RECEIVE_DATA", payload: response.rates });
+		});
+});
+
+export default store;
--- src/client/app/reducers/AllReducers.ts
+++ src/client/app/reducers/AllReducers.ts
@@ -3,11 +3,13 @@ import { combineReducers } from "redux";
 import conversions from "./ConversionsReducer";
 import rateOptions from "./RateOptions";
 import history from "./ConvertHistoryReducer";
+import rates from "./RatesReducer";
 
 const allReducers = combineReducers({
 	conversions,
 	rateOptions,
 	history,
+	rates,
 });
 
 export default allReducers;
